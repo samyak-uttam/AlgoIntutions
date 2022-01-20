6bd54364da3b027e784817d4c4cd27ef01f9b91e
Abhishek <arya2001abhishek@gmail.com>
2022-01-20 18:40:07 +0530

    add starter files and basic structure

--- app.js
+++ app.js
@@ -4,13 +4,42 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home");
+  // default question here, just for temporary time
+let questionsList = [
+  {
+    title: "Check children-sum property in a binary tree",
+    difficultTag: "easy",
+    topicsTag: ["array", "BinaryTree"],
+    description: "Given the root of a binary tree, determine if the binary tree holds children-sum property. For a tree to satisfy the children-sum property, each node’s value should be equal to the sum of values at its left and right subtree."
+  }, {
+    title: "Check children-sum property in a binary tree",
+    difficultTag: "easy",
+    topicsTag: ["array", "BinaryTree"],
+    description: "Given the root of a binary tree, determine if the binary tree holds children-sum property. For a tree to satisfy the children-sum property, each node’s value should be equal to the sum of values at its left and right subtree."
+  }, {
+    title: "Check children-sum property in a binary tree",
+    difficultTag: "easy",
+    topicsTag: ["array", "BinaryTree"],
+    description: "Given the root of a binary tree, determine if the binary tree holds children-sum property. For a tree to satisfy the children-sum property, each node’s value should be equal to the sum of values at its left and right subtree."
+  }, {
+    title: "Check children-sum property in a binary tree",
+    difficultTag: "easy",
+    topicsTag: ["array", "BinaryTree"],
+    description: "Given the root of a binary tree, determine if the binary tree holds children-sum property. For a tree to satisfy the children-sum property, each node’s value should be equal to the sum of values at its left and right subtree."
+  }
+];
+
+  res.render("home", {
+    questionsList
+  });
 });
 
 app.listen(3000, function() {
