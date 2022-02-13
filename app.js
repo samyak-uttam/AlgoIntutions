fd6df8fc444fbd908da659201e56ffc4838a161a
Abhishek <arya2001abhishek@gmail.com>
2022-02-13 20:16:19 +0530

    add dotenv in app

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const dbOperations = require(__dirname + '/utils/dbOperations.js');
